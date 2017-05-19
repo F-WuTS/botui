b28cbe66cc67a9eea8679ccd53843f5fb48600f3
Christoph Heiss <me@christoph-heiss.me>
2017-05-19 14:23:12 +0200

    Removed network panel code

--- src/SettingsWidget.cpp
+++ src/SettingsWidget.cpp
@@ -26,38 +26,19 @@ SettingsWidget::SettingsWidget(Device *device, QWidget *parent)
 	ui->setupUi(this);
 	performStandardSetup(tr("Settings"));
 
-  #ifdef NETWORK_ENABLED
-  ui->network->setEnabled(true);
-  #else
-  ui->network->setEnabled(false);
-  #endif
-
-	connect(ui->network, SIGNAL(clicked()), SLOT(network()));
 	connect(ui->comm, SIGNAL(clicked()), SLOT(comm()));
 	connect(ui->channels, SIGNAL(clicked()), SLOT(channels()));
 	connect(ui->gui, SIGNAL(clicked()), SLOT(gui()));
-	connect(ui->calibrate, SIGNAL(clicked()), SLOT(calibrate()));
-	connect(ui->language, SIGNAL(clicked()), SLOT(language()));
-  connect(ui->update, SIGNAL(clicked()), SLOT(update()));
-  connect(ui->hideUi, SIGNAL(clicked()), SLOT(hideUi()));
   connect(ui->battery, SIGNAL(clicked()), SLOT(battery()));
   connect(ui->cameraView, SIGNAL(clicked()), SLOT(cameraView()));
 
 	//TODO show buttons once the widgets are fixed
-	//ui->network->setVisible(false);
 	ui->comm->setVisible(false);
 }
 
 SettingsWidget::~SettingsWidget()
 {
 	delete ui;
-}
-
-void SettingsWidget::network()
-{
-#ifdef NETWORK_ENABLED
-  RootController::ref().presentWidget(new NetworkSettingsWidget(device()));
-#endif
 }
 
 void SettingsWidget::comm()
@@ -75,26 +56,9 @@ void SettingsWidget::gui()
 	RootController::ref().presentWidget(new GuiSettingsWidget(device()));
 }
 
-void SettingsWidget::calibrate()
-{
-	Calibrate::calibrate();
-}
-
 void SettingsWidget::language()
 {
   RootController::ref().presentWidget(new LanguageWidget(device()));
-}
-
-void SettingsWidget::update()
-{
-  WallabyUpdateWidget *const updateWidget = new WallabyUpdateWidget(device());
-  RootController::ref().presentWidget(updateWidget);
-  updateWidget->refresh();
-}
-
-void SettingsWidget::hideUi()
-{
-  RootController::ref().minimize();
 }
 
 void SettingsWidget::battery()
